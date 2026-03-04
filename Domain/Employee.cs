8d72c855566dcd0e5f9e9e674786fcdd1dfc7d12
LucasKgarose <lkseala@gmail.com>
2026-03-04 10:59:44 +0200

    added the domain class

---
+++ Domain/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmployeeEmail { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
