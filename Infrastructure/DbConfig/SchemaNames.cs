c1c7b75fac87cdfc3d93dc7fa47a7f1a7265f72b
LucasKgarose <lkseala@gmail.com>
2026-03-04 14:46:58 +0200

    Added schema names and extended the Efcore Identity classes

---
+++ Infrastructure/DbConfig/SchemaNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.DbConfig
+{
+    internal class SchemaNames
+    {
+        public static string Security = nameof(Security);
+        public static string HR = nameof(HR);
+    }
+}
---
+++ Infrastructure/Models/ApplicationRoleClaim.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Models
+{
+    public class ApplicationRoleClaim : IdentityRoleClaim<string>
+    {
+        public string Description { get; set; }
+        public string Group { get; set; }
+    }
+}
